15e6184312c9899de2fb83aa477162086586a69f
Your Name <you@example.com>
2025-05-27 00:17:37 +0530

    jwt flask

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from routes.auth import auth_bp
 
 def create_app():
     app = Flask(__name__)
-    CORS(app)
+    CORS(app, origins=["http://localhost:5173"], supports_credentials=True)
     app.config.from_object(Config)
 
     init_extensions(app)
